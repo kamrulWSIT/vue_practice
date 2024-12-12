c6ae177eec5bc6d6841c2047e42cc1375ddd15b1
kamrulWSIT <kamrul@workspaceit.com>
2024-12-12 13:34:19 +0600

    composition api with ref

--- vue_using_npm/vue_crash/src/App.vue
+++ vue_using_npm/vue_crash/src/App.vue
@@ -1,30 +1,32 @@
 <script>
+import { ref } from 'vue';
+
 export default {
-  data() {
-    return {
-      name: "Kamrul Hasan",
-      status: 'pending',
-      tasks: ['one', 'two', 'three'],
-      link: 'https://github.com/kamrulWSIT/vue_practice'
-    }
-  },
-  methods: {
-    toggleStatus() {
-      if (this.status === 'active') {
-        this.status = 'pending'
+  setup() {
+    const name = ref('Kamrul Hasan');
+    const status = ref('active');
+    const tasks = ref(['one', 'two', 'three']);
+    const toggleStatus = () => {
+      if (status.value === 'active') {
+        status.value = 'pending'
       }
-      else if (this.status === 'pending') {
-        this.status = 'inactive'
+      else if (status.value === 'pending') {
+        status.value = 'inactive'
       }
       else {
-        this.status = 'active'
+        status.value = 'active'
       }
+    };
+    return  {
+      name, status, tasks, toggleStatus,
     }
-  }
+  },
 }
+
 </script>
 
 <template>
+
   <h1>
     {{ name }}
   </h1>
@@ -32,16 +34,14 @@ export default {
   <p v-else-if="status === 'pending'">user is pending</p>
   <p v-else>user is inactive</p>
 
+
   <h3>Tasks</h3>
   <ul>
     <li v-for="task in tasks" :key="task">{{ task }}</li>
   </ul>
-  <a v-bind:href="link">Click for project repo</a>
-  <a :href="link">Click for project repo</a>
-  <br>
-  <br>
+ 
 
-  <button v-on:click="toggleStatus">click me</button>
-  <button @click="toggleStatus">click me</button>
+  <button @click="toggleStatus">change status</button>
+
 
 </template>
---
+++ vue_using_npm/vue_crash/src/App2.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "Kamrul Hasan",
+      status: 'pending',
+      tasks: ['one', 'two', 'three'],
+      link: 'https://github.com/kamrulWSIT/vue_practice'
+    }
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending'
+      }
+      else if (this.status === 'pending') {
+        this.status = 'inactive'
+      }
+      else {
+        this.status = 'active'
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>
+    {{ name }}
+  </h1>
+  <p v-if="status === 'active'">user is active</p>
+  <p v-else-if="status === 'pending'">user is pending</p>
+  <p v-else>user is inactive</p>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <a v-bind:href="link">Click for project repo</a>
+  <a :href="link">Click for project repo</a>
+  <br>
+  <br>
+
+  <button v-on:click="toggleStatus">click me</button>
+  <button @click="toggleStatus">click me</button>
+
+</template>
