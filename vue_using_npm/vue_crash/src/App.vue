a1186045ccd251f7a2a9e03d47cc96a43dacb5d6
kamrulWSIT <kamrul@workspaceit.com>
2024-12-12 12:20:27 +0600

    vue create with npm and learn v-if, v-else-if, v-else derective

---
+++ vue_using_npm/vue_crash/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "Kamrul Hasan",
+      status: 'pending'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>
+    {{ name }}
+  </h1>
+  <p v-if="status === 'active'">user is active</p>
+  <p v-else-if="status === 'pending'">user is pending</p>
+  <p v-else>user is inactive</p>
+</template>
